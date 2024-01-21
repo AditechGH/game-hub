46b3454114a745b5a4639923c8d8c14412073d8c
Adinan Alhassan <aditech5170@gmail.com>
2024-01-21 21:06:11 +0000

    Setup Routing

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -5,14 +5,17 @@ import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 import Emoji from "./Emoji";
+import { Navigate, useNavigate } from "react-router-dom";
 
 interface Props {
   game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
+  const navigate = useNavigate();
+
   return (
-    <Card>
+    <Card cursor={"pointer"} onClick={() => navigate(`/games/${game.id}`)}>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <HStack justifyContent={"space-between"} marginBottom={3}>
@@ -21,8 +24,9 @@ const GameCard = ({ game }: Props) => {
           />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize={"2xl"}>{game.name} <Emoji rating={game.rating_top} /></Heading>
-
+        <Heading fontSize={"2xl"}>
+          {game.name} <Emoji rating={game.rating_top} />
+        </Heading>
       </CardBody>
     </Card>
   );
---
+++ src/components/HomePage.tsx
@@ -0,0 +1,32 @@
+import { Box, Flex, GridItem, Show } from "@chakra-ui/react";
+import GameGrid from "./GameGrid";
+import GameHeading from "./GameHeading";
+import GenreList from "./GenreList";
+import PlatformSelector from "./PlatformSelector";
+import SortSelector from "./SortSelector";
+
+const HomePage = () => {
+  return (
+    <>
+      <Show above="lg">
+        <GridItem area="aside" paddingX={5}>
+          <GenreList />
+        </GridItem>
+      </Show>
+      <GridItem area="main">
+        <Box paddingLeft={2}>
+          <GameHeading />
+          <Flex marginBottom={5}>
+            <Box marginRight={5}>
+              <PlatformSelector />
+            </Box>
+            <SortSelector />
+          </Flex>
+        </Box>
+        <GameGrid />
+      </GridItem>
+    </>
+  );
+};
+
+export default HomePage;
